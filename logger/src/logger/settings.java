4f936193cf105607cba21a58e63ee467d6cca32a
pslowater@gmail.com <pslowater@gmail.com@602c6021-25ee-92a6-eefd-7fd8a19674d7>
2011-02-21 12:01:47 +0000

    Added retrieve of mode which determines how the Instantaneous Power is calculated

--- logger/src/logger/settings.java
+++ logger/src/logger/settings.java
@@ -11,32 +11,59 @@ import org.apache.commons.configuration.*;
  */
 public class settings {
 
+    public static final int AVERAGE = 0;
+    public static final int PEAK = 1;
+    public static final int INSTANT = 2;
+
     public static String getPort() {
-        String port = config.getInstance().getFactory().getString("PORT","BAD");
+        String port = config.getInstance().getFactory().getString("PORT", "BAD");
         return port;
     }
+
     public static String getDailyFile() {
-        String port = config.getInstance().getFactory().getString("DAILY","c:\\temp");
+        String port = config.getInstance().getFactory().getString("DAILY", "c:\\temp");
         return port;
     }
+
     public static String getCurl() {
-        String port = config.getInstance().getFactory().getString("cURL","BAD");
+        String port = config.getInstance().getFactory().getString("cURL", "BAD");
         return port;
     }
+
     public static String getURL() {
-        String port = config.getInstance().getFactory().getString("URL","BAD");
+        String port = config.getInstance().getFactory().getString("URL", "BAD");
         return port;
     }
+
     public static String getAPIKey() {
-        String port = config.getInstance().getFactory().getString("APIKey","BAD");
+        String port = config.getInstance().getFactory().getString("APIKey", "BAD");
         return port;
     }
+
     public static String getSystemId() {
-        String port = config.getInstance().getFactory().getString("SystemId","BAD");
+        String port = config.getInstance().getFactory().getString("SystemId", "BAD");
         return port;
     }
+
     public static int getSleep() {
-        int time = config.getInstance().getFactory().getInt("SLEEP",60000);
+        int time = config.getInstance().getFactory().getInt("SLEEP", 60000);
         return time;
+    }
+/**
+ * Returns how the Instantaneous Power is calculated
+ * instant - value at the poll interval
+ * average - average value between the poll interval
+ * peak - peak value obtained between poll intervals
+ * @return
+ */
+    public static int getMode() {
+        String mode = config.getInstance().getFactory().getString("mode", "instant");
+        if (mode.contentEquals("instant")) {
+            return INSTANT;
+        }
+        if (mode.contentEquals("average")) {
+            return AVERAGE;
+        }
+        return PEAK;
     }
 }
