c96937690e8d18aedd19356e3fc0347eb46b65f9
pslowater@gmail.com <pslowater@gmail.com@602c6021-25ee-92a6-eefd-7fd8a19674d7>
2011-02-22 10:04:00 +0000

    Renamed the log writer and added a "line" writer

--- logger/src/logger/LogWriter.java
+++ logger/src/logger/LogWriter.java
@@ -20,13 +20,24 @@ public class LogWriter {
 
     Logger logger = Logger.getLogger(this.getClass().getName());
 
-    public void WriteLine(Date date, int Now, double eTotal, double eToday, double Temp) {
+    public void WriteDataToLog(Date date, int Now, double eTotal, double eToday, double Temp) {
         String x = String.format("%s %04d %07.1f %04.1f %03.1f\n", utils.getTime2(date), Now, eTotal, eToday, Temp);
         logger.info("Write Values " + x);
         try {
             FileWriter write = new FileWriter(settings.getDailyFile()+"\\" + utils.getFileDate(date) + ".data", true);
             PrintWriter print_line = new PrintWriter(write);
             print_line.printf("%s %04d %07.1f %04.1f %03.1f\n", utils.getTime2(date), Now, eTotal, eToday, Temp);
+            print_line.close();
+
+        } catch (Exception ex) {
+            logger.error(ex);
+        }
+    }
+    public void WriteLine(Date date) {
+        try {
+            FileWriter write = new FileWriter(settings.getDailyFile()+"\\" + utils.getFileDate(date) + ".data", true);
+            PrintWriter print_line = new PrintWriter(write);
+            print_line.printf("=========================================================================\n");
             print_line.close();
 
         } catch (Exception ex) {
